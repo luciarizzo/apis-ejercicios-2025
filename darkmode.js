abfacec76fff446b654aed6b66d22bcd9319f848
Gabriela Méndez <gabrielamendezg@outlook.com>
2025-08-11 22:08:12 -0300

    toggle dark mode

--- darkmode.js
+++ darkmode.js
@@ -5,8 +5,10 @@ function botonCambiar() {
     if (body.classList.contains('light-mode')) {
         body.classList.remove('light-mode');
         body.classList.add('dark-mode');
+        botonMode.checked = true;
     } else {
         body.classList.remove('dark-mode');
         body.classList.add('light-mode');
+        botonMode.checked = false;
     }
 }
